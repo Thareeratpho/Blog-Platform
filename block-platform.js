ccddd803d9ed45ed73c696d0a706753435da852f
Thareeratpho <163884396+Thareeratpho@users.noreply.github.com>
2024-03-30 19:04:28 +0700

    Updates

--- block-platform.js
+++ block-platform.js
@@ -1,8 +1,31 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+//nav
+function navFunction() {
+    var nav = document.getElementById("myLinks");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
     } else {
-        x.style.display = "block";
+        nav.style.display = "block";
     }
 }
+
+//log in
+function loginFunction() {
+    var modal = document.getElementById("login-modal");
+    var btn = document.getElementById("loginbutton");
+    var span = document.getElementsByClassName("close")[0];
+    if (btn && modal) {
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+    }
+    if (span && modal) {
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+};
